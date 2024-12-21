72449155b469057c7d3e2f4687f6d9bfc90ed746
volkan <nzaro19@gmail.com>
2024-12-21 15:39:25 +0300

    Added end points for the API

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -22,6 +25,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::prefix('api')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/cart/add', [CartController::class, 'addToCart']);
+    Route::post('/cart/remove', [CartController::class, 'removeFromCart']);
+    Route::get('/cart', [CartController::class, 'viewCart']);
+    Route::post('/order/place', [OrderController::class, 'placeOrder']);
+    Route::get('/orders', [OrderController::class, 'viewOrders']);
 });
 
 require __DIR__.'/auth.php';
