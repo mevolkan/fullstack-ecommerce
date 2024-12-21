64726fd35c15b55c7ef26a3ca4d0a4215c77ad1f
volkan <nzaro19@gmail.com>
2024-12-21 15:47:29 +0300

    added order controller methods

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class OrderController extends Controller
 {
-    //
+    public function placeOrder(Request $request) {
+        $cart = json_decode(Redis::get('cart'), true);
+        $order = Order::create([
+            'user_id' => $request->user()->id,
+            'details' => json_encode($cart)
+        ]);
+        Redis::del('cart');
+        return response()->json(['message' => 'Order placed successfully']);
+    }
+
+    public function viewOrders() {
+        $orders = Order::where('user_id', auth()->id())->get();
+        return response()->json($orders);
+    }
 }
