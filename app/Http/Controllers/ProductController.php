5fd7e273470502f6b778fdfde229bf5c0b0089b7
volkan <nzaro19@gmail.com>
2024-12-21 15:40:59 +0300

    added product controller method to list all products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(Product::all());
+    }
 }
